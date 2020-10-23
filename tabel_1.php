b0afed81162bc1109ee7b4ff5a3679f111db2f6e
pablo-jenne <61419450+pablo-jenne@users.noreply.github.com>
2020-10-23 14:14:26 +0200

    update
    een update van tabel_1.php

--- tabel_1.php
+++ tabel_1.php
@@ -23,15 +23,26 @@ if(isset($_POST["sensor"]))
     }
 }
 
+        $query = "SELECT * FROM Project_IoT_1";
+        $result=$mysqli->query($query); 
 
 
 
+        if (isset($_POST['Search'])) {
+		
+            $search_query = $_POST['Search'];
+    
+            $query = "SELECT * FROM Project_IoT_1 WHERE Name='$search_query'";
+            $result=$mysqli->query($query);
+            $count = mysqli_num_rows($result);
+    
+            if ($count == 0) {
+                echo "geen resultaat";
+            }
+        }
 
 
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -41,15 +52,38 @@ if(isset($_POST["sensor"]))
     <title>Document</title>
 </head>
 <body>
-    <form action="./tabel_1.php" method="post">
 
-    Sensor1 of Sensor2? <input type="text" name="sensor"><br><br>
-    waarde              <input type="text" name="waarde"><br><br>
+<form action="tabel_1.php" method="post">
+		<input type="text" name="Search" placeholder="Search"><br>
+		<input type="submit" name="zoek"><br>
+	
+</form>
 
-    <input type="submit" name="submit" >
 
+<table align="center" border="1px" style="width:600px; line-height:40px;"> 
+	<tr> 
+		<th colspan="4"><h2> Meeting sensoren </h2></th> 
+		</tr> 
+			  <th> Timestamp </th> 
+			  <th> waarde </th> 
+			  <th> Sensor_ID </th> 
+			  
+		</tr> 
 
+        <?php while($rows=mysqli_fetch_assoc($result)) 
+		{ 
+			 ?>
+			<tr>  
+				<td><?php echo $rows['Timestamp']; ?></td> 
+				<td><?php echo $rows['Value']; ?></td> 
+				<td><?php echo $rows['Sensor_ID']; ?></td>
 
-    </form>
+             </tr>   
+		<?php		  
+		}
+           ?> 
+	</table>
+    
 </body>
+
 </html>
