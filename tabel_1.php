f42117e2b5abfa451758eeca045d91519e3e2f73
pablo-jenne <61419450+pablo-jenne@users.noreply.github.com>
2020-10-30 13:49:44 +0100

    Add files via upload

--- tabel_1.php
+++ tabel_1.php
@@ -2,8 +2,47 @@
 <?php
 
 include "./database_connection.php";
+include "./API.php";
 
-if(isset($_POST["sensor"]))
+
+if(isset($temp)) // hier komt er data binnen via de API (temperatuur)
+{
+    $temperatuur = $temp;
+    $temperatuur_graden = $temperatuur - 273.15;
+    $sensor_1 = 1;
+
+    $stmt = $mysqli->prepare('INSERT INTO Project_IoT_1 ( Value,Sensor_ID) VALUES (?,?);');
+    $stmt->bind_param('ss', $temperatuur_graden, $sensor_1 );
+
+
+    if(!$stmt->execute()){
+        echo $mysqli->error;
+        echo "NOK";
+        echo "</br>";
+        echo "<a href='./tabel_1.php'>Oei er is een foutje, ga terug</a>";
+        }
+
+}
+
+if(isset($humidity)) // hier komt er data binnen via de API (luchtvochtigheid)
+{
+    $vochtigheid = $humidity;
+    $sensor_2 = 2;
+
+    $stmt = $mysqli->prepare('INSERT INTO Project_IoT_1 ( Value,Sensor_ID) VALUES (?,?);');
+    $stmt->bind_param('ss', $vochtigheid, $sensor_2 );
+
+
+    if(!$stmt->execute()){
+        echo $mysqli->error;
+        echo "NOK";
+        echo "</br>";
+        echo "<a href='./tabel_1.php'>Oei er is een foutje, ga terug</a>";
+        }
+
+}
+
+if(isset($_POST["sensor"])) // deze input komt van het form om handmatig data in te vullen
 {
     if(isset($_POST["waarde"]))
     {
@@ -14,7 +53,7 @@ if(isset($_POST["sensor"]))
         $stmt->bind_param('ss', $waarde, $sensor );
 
         if(!$stmt->execute()){
-        echo $mysqli -> error;
+        echo $mysqli->error;
         echo "NOK";
         echo "</br>";
         echo "<a href='./tabel_1.php'>Oei er is een foutje, ga terug</a>";
@@ -31,8 +70,8 @@ if(isset($_POST["sensor"]))
         if (isset($_POST['Search'])) {
 		
             $search_query = $_POST['Search'];
-    
-            $query = "SELECT * FROM Project_IoT_1 WHERE Name='$search_query'";
+                
+            $query = "SELECT * FROM Project_IoT_1 WHERE Value='$search_query' OR Timestamp='$search_query' OR Sensor_ID='$search_query'";
             $result=$mysqli->query($query);
             $count = mysqli_num_rows($result);
     
@@ -49,6 +88,7 @@ if(isset($_POST["sensor"]))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="600"> 
     <title>Document</title>
 </head>
 <body>
@@ -87,3 +127,5 @@ if(isset($_POST["sensor"]))
 </body>
 
 </html>
+
+
